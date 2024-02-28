4b34902fac995a23237a753dc2d601f343a76613
kpblc2000 <kpblc2000@gmail.com>
2024-02-28 23:21:32 +0300

    Тестовая команда - просто убедиться, что все плюс-минус работает

---
+++ KpblcNcUpdate/NCadCommands/AboutCmd.cs
@@ -0,0 +1,13 @@
+﻿using Teigha.Runtime;
+
+namespace KpblcNcUpdate.NCadCommands
+{
+    public static class AboutCmd
+    {
+        [CommandMethod("kpblc-about")]
+        public static void KpblcAboutCommand()
+        {
+            System.Windows.Forms.MessageBox.Show("About Command");
+        }
+    }
+}
